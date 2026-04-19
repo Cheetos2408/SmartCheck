19a9f9d5e8400dc7483ab0af763d3c2f8add1e19
Cheetos2408 <omuralievsanjar29@gmail.com>
2026-04-20 01:33:42 +0600

    fix package.json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SmartCheck/',   // точное название репозитория на GitHub
+  base: '/SmartCheck/',  
 })
